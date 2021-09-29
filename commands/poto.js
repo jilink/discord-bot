170668a73529b94e2d5dfd3062133c3acecfd80e
plicence <p.licencedev@gmail.com>
2021-09-30 01:56:47 +0200

    Amélioration

--- commands/poto.js
+++ commands/poto.js
@@ -1,7 +1,9 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { isMessageComponentDMInteraction } = require('discord-api-types/utils/v9');
 const { clientId, guildId, token } = require('../config.json');
-let mots = ['chocolat', 'café', 'eau', 'voiture'];
+let mots = ['chocolat'];
+const map = new Map();
+var mot1 = ""; 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("poto")
@@ -25,18 +27,43 @@ module.exports = {
         filter,
         time: 5000,
         min: 1,
+		dispose: true,
       });
       collector.on("collect", (reaction, user) => {
-        ids.add(user.id);
+		if(reaction.emoji.name ==='🚫' && user.id === interaction.user.id){ //ajouter l'id de l'auteur pour bloquer les autres 
+			message.edit('Le jeu a été arrêté par son auteur');
+			return false;
+		}
+		ids.add(user.id);
+		let jsp = undefined; // conteneur de l'ensemble des id pour chaque clés
+		if(map.get(reaction.emoji.name) != undefined){
+			jsp = Array.from([]).concat(map.get(reaction.emoji.name)).concat(user.id) //si la clé est déjà associée
+		}
+		else {
+			jsp = Array(user.id)
+		}
+		map.set(reaction.emoji.name, jsp);
         console.log(`Reaction collected from ${reaction.emoji.name}, ${user}`);
       });
 
+	  collector.on("remove", (reaction, user) => { // enlève une réaction du vecteur en cas de retrait de réaction
+		console.log("remove reaction");
+		ids.delete(user.id);
+		let jsp = undefined; // conteneur de l'ensemble des id pour chaque clés
+		const tmp = map.get(reaction.emoji.name);
+		tmp.splice(tmp.indexOf(user.id), 1);
+        console.log(`Reaction removed from ${reaction.emoji.name}, ${user}`);
+      });
+
+
       collector.on("end", (collected, reason) => {
-        //console.log(ids);
-        let array = Array.from(ids);
-        //console.log(array);
+		if(map.get("👍") === undefined || map.get("👍").length === 0 ) { // vérifie qu'au moins une personne a voté pour jouer, sinon le jeu se termine
+			message.edit("Personne ne veut jouer");
+			return false;
+		}
+		
+		let array = Array.from(ids);
         message.edit("Début du jeu");
-				console.log("users", message.reactions.cache.get('👍').users)
         message.reactions
           .removeAll()
           .catch((error) =>
@@ -45,8 +72,9 @@ module.exports = {
         //console.log( `tableau est  ${tab[0]}`);
         const maitre = array[Math.floor(Math.random() * array.length)];
         const traitre = array[Math.floor(Math.random() * array.length)];
-        const mot1 = mots[Math.floor(Math.random() * mots.length)];
-        maitre &&
+        mot1 = mots[Math.floor(Math.random() * mots.length)];
+        console.log(mot1);
+		maitre &&
           client.users.cache
             .get(maitre)
             .send(`Vous êtes le maître. Le mot est : ${mot1}`);
@@ -54,7 +82,16 @@ module.exports = {
           client.users.cache
             .get(traitre)
             .send(`Vous êtes le traître. Le mot est : ${mot1}`);
+			map.clear();
       });
+	  const filter2 = m => m.content.includes(mot1);
+	  const collectormessage = interaction.channel.createMessageCollector({filter2});
+	  //let mrep = undefined;	 
+	  collectormessage.on('collect', m => {
+		if(m.content === mot1)  
+			m.reply({content : 'Vous avez trouvé le mot', fetchReply: true})
+			m.react("👍").then(() => m.react("🚫"));
+		});
     } catch (error) {
       console.log(error);
       message.reply(`Personne n'a voulu jouer`);
