067573e16de6bd43ad6af91886d935deff2efc39
plicence <p.licencedev@gmail.com>
2021-09-29 22:02:59 +0200

    Version qui marche mais pas vraiment

---
+++ commands/poto.js
@@ -0,0 +1,85 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { isMessageComponentDMInteraction } = require('discord-api-types/utils/v9');
+const { clientId, guildId, token } = require('../config.json');
+let mots = ['chocolat', 'café', 'eau', 'voiture'];
+module.exports = {
+	
+	data: new SlashCommandBuilder()
+		.setName('poto')
+		.setDescription('Lance le jeu du traitre'),
+	async execute(interaction, client) {
+		const message = await interaction.reply({ content : 'Qui veut jouer ?', fetchReply: true});
+		var ids = new Set();
+		message.react('👍')
+			.then(() => message.react('🚫'));
+		
+		try{
+
+			const filter = (reaction, user) => {
+				//console.log(user);
+				return['👍', '🚫'].includes(reaction.emoji.name) && user.id != clientId;
+			}
+			const collector = message.createReactionCollector({filter,  time: 5000, min:1});
+			collector.on('collect', (reaction, user) => {
+				ids.add(user.id);
+				console.log(`Reaction collected from ${reaction.emoji.name}, ${user}`);
+				
+
+			});
+
+			collector.on('end', (collector, reason) => {
+				//console.log(ids);
+				let array = Array.from(ids);
+				//console.log(array);	
+				message.edit('Début du jeu');
+					message.reactions.removeAll()
+						.catch(error => console.error('Erreur lors de la prise de réactions', error));
+				//console.log( `tableau est  ${tab[0]}`);
+				const maitre = array[Math.floor(Math.random()*array.length)];	
+				const traitre = array[Math.floor(Math.random()*array.length)];	
+				const mot1 = mots[Math.floor(Math.random()*array.length)];
+				client.users.cache.get(maitre).send(`Vous êtes le maître. Le mot est : ${mot1}`);
+				client.users.cache.get(traitre).send(`Vous êtes le traître. Le mot est : ${mot1}`);
+
+			});
+
+		}
+		catch(error) {
+
+			console.log(error);
+			message.reply(`Personne n'a voulu jouer`);
+		}
+		
+		/*message.awaitReactions({filter, max: 10, time: 5000})
+			.then(collected => {
+				const reaction = collected.first();
+				const user = col;
+				console.log(user);
+				const channel = interaction.channel	
+				if (reaction.emoji.name === '👍'){
+					
+					message.edit('J ai changé d avis');
+					message.reactions.removeAll()
+						.catch(error => console.error('Erreur lors de la suppression de réactions', error));	
+
+				}
+				else if (reaction.emoji.name === '🚫'){
+					
+					message.reply('Fin');
+				}
+
+			})
+			.catch(collected => {
+				message.reply("Ouille");
+
+
+			});
+*/
+
+
+
+
+	},
+	
+	
+};
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require("dotenv").config(); //initialize dotenv
-const Discord = require("discord.js"); //import discord.js
-
-const client = new Discord.Client({
-  intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS"],
+const fs = require('fs');
+//const Discord = require("discord.js"); //import discord.js
+const {Client, Collection ,Intents} = require('discord.js');
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS], partials : ["CHANNEL"]
 }); //create new client
 const jimmy_uid = "378990039109206017";
 const quiz_channelId = "852249134282178600";
@@ -13,36 +14,49 @@ client.on("ready", () => {
   // client.users.cache.find(user => user.id === my_uid)
 });
 
-client.on("message", (msg) => {
-  console.log("message", msg.channelId, "me", jimmy_channelId);
-  if (jimmy_channelId === msg.channelId) {
-    const is_simple_poll = msg.author.id == "324631108731928587";
-    const author_tag = `<@${msg.author.id}>`;
-    if (is_simple_poll) {
-      msg.reply(
-        "wesh y a un sondage @here ceux qui répondent pas leurs noms sont ajoutés et ils seront ban dans moins de 24h (pas une blague)"
-      );
-    }
-    if (msg.author.bot) return;
-    const msg_lower = msg.content.toLowerCase();
-    if (msg_lower.includes("pierre")) {
-      msg.reply(
-        "Les gars vous parlez de pierre ? cest un génie ce mec, regardez : Pierre, 2 + 2 = ?"
-      );
-    }
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+
+  const command = require(`./commands/${file}`);
+
+  client.commands.set(command.data.name, command);
+
+}
+//console.log(client.commands)
+/*client.on("messageCreate", message => {
+
+  console.log(message);
+  if (message.author.id == '430374755363979264') {
+      message.reply("EUh");
+
   }
-});
+
+})*/
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
 
-	const { commandName } = interaction;
+  //console.log(interaction);
+  if(!interaction.isCommand()) return;
 
-	if (commandName === 'react') {
-		const message = await interaction.reply({ content: 'You can react with Unicode emojis!', fetchReply: true });
-		message.react('😄');
-	}
+  const command = client.commands.get(interaction.commandName);
+  if(!command) return;
+
+  try {
+    await command.execute(interaction, client);
+    console.log("here");
+
+  } catch(error) {
+    console.error(error);
+    await interaction.reply({ content : 'Ther was an error while executing this command!'});
+  }
+
+
+
 });
+
+
 
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
